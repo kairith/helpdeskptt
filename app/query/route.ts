84fe7cd9e61ef226daeb0b7ef4293bf727d330aa
channdararith <dararithzone@gmail.com>
2025-05-28 09:44:49 +0700

    Update Components

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,7 +4,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
   const data = await sql`
-    SELECT invoices.amount, customers.name
+    SELECT DISTINCT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
